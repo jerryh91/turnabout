219016244846b936881545d77c65038ed5d41772
mb35534 <marcus.a.bennett@gmail.com>
2015-07-04 15:42:49 -0500

    cosmetic cleanup

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -3,16 +3,6 @@ var router = express.Router();
 
 module.exports = function(passport){
 
-    //sends successful login state back to angular
-    router.get('/success', function(req, res){
-        res.send({state: 'success', user: req.user ? req.user : null});
-    });
-
-    //sends failure login state back to angular
-    router.get('/failure', function(req, res){
-        res.send({state: 'failure', user: null, message: "Invalid username or password"});
-    });
-
     //log in
     router.post('/login', passport.authenticate('login', {
         successRedirect: '/auth/success',
@@ -24,6 +14,18 @@ module.exports = function(passport){
         successRedirect: '/auth/success',
         failureRedirect: '/auth/failure'
     }));
+
+     //sends successful login state back to angular
+    router.get('/success', function(req, res){
+        console.log("went through the /success GET");
+        res.send({state: 'success', user: req.user ? req.user : null});
+    });
+
+    //sends failure login state back to angular
+    router.get('/failure', function(req, res){
+        console.log("went through the /failure GET");
+        res.send({state: 'failure', user: null, message: "Invalid username or password"});
+    });
 
     //log out
     router.get('/signout', function(req, res) {
