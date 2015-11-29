24af08feb24e6ca7cf084bdebb186ee630de3885
mb35534 <marcus.a.bennett@gmail.com>
2015-11-29 13:50:36 -0600

    fixed typo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // app.js
 //
 
-var expsress = require('express');
+var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
